3f89614237221d385d98b0b7665bb1ccfa9690ea
SAGHIR-Imadeddine <sag.imadeddine@gmail.com>
2024-02-09 11:06:31 +0100

    HireMe

---
+++ database/migrations/2024_02_06_155022_create_resumes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resumes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_profile_id')->constrained()->onDelete('cascade');
+            $table->json('skills')->nullable();
+            $table->json('work_experiences')->nullable();
+            $table->json('educational_backgrounds')->nullable();
+            $table->json('languages')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resumes');
+    }
+};
---
+++ database/migrations/2024_02_06_155513_create_job_offers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_offers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->json('required_skills');
+            $table->enum('contract_type', ['remote', 'hybrid', 'full-time']);
+            $table->string('location');
+            $table->boolean('archive')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_offers');
+    }
+};
